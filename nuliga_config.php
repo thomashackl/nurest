d4850302df2b4aa04515edcb5899dab7560e4a20
Thomas Hackl <hackl@data-quest.de>
2021-12-28 16:25:42 +0100

    config file without sensitive entries

---
+++ nuliga_config.php
@@ -0,0 +1,16 @@
+<?php
+
+$nuLigaConfig = [
+    'api' => [
+        'base_url' => 'https://dartde-portal.liga.nu',
+        'client_id' => '<CLIENT_ID>',
+        'client_secret' => '<CLIENT_SECRET>'
+    ],
+    'db' => [
+        'host' => '<DATABASE_HOST>',
+        'user' => '<DATABASE_USER>',
+        'password' => '<DATABASE_PASSWORD>',
+        'name' => '<DATABASE_NAME>'
+    ]
+];
+
